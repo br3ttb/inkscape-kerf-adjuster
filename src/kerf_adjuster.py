85d81c3fccd5a1f961372131e2ac041845e461df
Brett <br3ttb@gmail.com>
2020-01-04 07:33:40 -0800

    updated readme, playing with kerf units

--- src/kerf_adjuster.py
+++ src/kerf_adjuster.py
@@ -49,8 +49,9 @@ that a laser cutter's beam has some width.
         Effect behaviour.
         Overrides base class' method.
         """
-	kerfpx = float(self.options.kerfmm) * 3.7795
-
+	kerfpx = float(self.options.kerfmm) #* 3.7795
+	inkex.debug(self.options.kerfmm)
+	inkex.debug(2*kerfpx)
 	#build the working objects
 	nodes = self.selected.values()
 	todo = []
@@ -68,14 +69,17 @@ that a laser cutter's beam has some width.
 			bodyMask = deepcopy(sel) 
 			bodyMaskId = self.uniqueId(id)
 			bodyMask.attrib["id"] = bodyMaskId
-			bodyMask.attrib["style"]="opacity:1;fill:#a0a0a0;fill-opacity:0;stroke:#ff0000;stroke-width:1;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.5"
+			bodyMask.attrib["style"]="opacity:1;fill:#a0a0a0;fill-opacity:0;stroke:#ff0000;stroke-width:0.5px;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.5"
 			parent.append(bodyMask)
 
 			#Create a copy that will be the StrokeToPath object
-			kerfObject = deepcopy(sel) 
+			kerfObject = deepcopy(sel)
+
 			kerfObjectId = self.uniqueId(id)
 			kerfObject.attrib["id"] = kerfObjectId
-			kerfObject.attrib["style"]="opacity:1;fill:#a0a0a0;fill-opacity:0;stroke:#000000;stroke-width:"+str(2*kerfpx)+";stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.5"
+			kerfObject.attrib["style"]="opacity:1;fill:#a0a0a0;fill-opacity:0;stroke:#000000;stroke-width:"+str(2*kerfpx)+"px;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:0.5"
+			inkex.debug(inkex.etree.tostring(sel, pretty_print=True))
+			inkex.debug(inkex.etree.tostring(kerfObject, pretty_print=True))
 			parent.append(kerfObject)
 			todo.append({'bodyId':bodyMaskId, 'kerfId':kerfObjectId})
 
